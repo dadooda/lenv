0e6a349f000ce597d95d34a8d1ac6da14dde50ea
Alex Fortuna <fortunadze@gmail.com>
2025-08-09 14:23:12 +0300

    [skip ci] Moving on

--- lenv.sh
+++ lenv.sh
@@ -10,64 +10,38 @@
 # Envfile basename we're looking for in the support directories.
 _LENV_BN="env.sh"
 
-#----------------------------- Functions
-
-# Locate and print the envfile path. Return 1 if not found.
-_lenv_fn() {
-  # Search for a readable envfile up the tree.
-  # Testing: `_LENV_FN_ROOT` is an optional root specifier.
-  local BN="${_LENV_BN}"
-  local D=`realpath "${PWD}"` || return 1
-
-  while [ -n "${D#${_LENV_FN_ROOT:-}}" ]; do
-    for TRY in "`realpath "$D/../${D##*/}_support"`/${BN}" "`realpath "${D}/../_support"`/${BN}"; do
-      if [ -r "${TRY}" ]; then
-        echo "${TRY}"
-        return 0
-      fi
-    done
-
-    D=${D%/*}
-  done
-
-  for TRY in "`realpath "$D/../${D##*/}_support"`/${BN}" "`realpath "${D}/../_support"`/${BN}"; do
-    if [ -r "${TRY}" ]; then
-      echo "${TRY}"
-      return 0
-    fi
-  done
-
-  echo "Error: Support directory with a readable \`${BN}\` not found" >&2
-  return 1
-}
-
 # Load the envfile from the support directory.
 lenv() {
-  # NOTE: `local` assignment yields a non-error even if the command has failed. Hence an additional check.
-  local FN=$(_lenv_fn); [ -n "${FN}" ] || return 1
-  [ -r "${FN}" ] || {
+  echo -e "\e[1;32m${FUNCNAME[0]}():\e[0m hey" >&2
+  local FN
+  FN=$(_lenv_fn) || return 1
+  echo -e "\e[1;32m${FUNCNAME[0]}():\e[0m cp 1:{cp 1}" >&2
+
+  [[ -r ${FN} ]] || {
     echo "Error: File is not readable: ${FN}" >&2
     return 1
   }
 
   # NOTE: The logic below is somewhat "tilted" to generate prettier `set -x` output.
-  if [ "${VERBOSE:-}" = "!" ]; then
+  if [[ ${VERBOSE:-} = "!" ]]; then
     set -x
-    . "$FN"
+    . "${FN}"
     set +x
   else
-    echo "Loading '$FN'"
-    . "$FN"
+    echo "Loading: ${FN}"
+    . "${FN}"
   fi
 }
 
 # Temporarily step into the support directory via `pushd`.
 lecd() {
-  local FN=$(_lenv_fn); [ -n "${FN}" ] || return 1
+  local FN
+  FN=$(_lenv_fn) || return 1
+
   local DIR=${FN%/*}
 
   echo
-  echo "Stepping into: ${DIR}"
+  echo "Cd to: ${DIR}"
   echo "Do a \`popd\` to return"
   echo
 
@@ -78,29 +52,33 @@ lecd() {
 #
 # $1: (optional) module name.
 leed() {
-  if [ $# != 0 ]; then
+  local FN
+  local MOD
+  local VN
+
+  if [[ $# -ge 1 ]]; then
     # Edit the module.
 
-    local MOD=$1
-    local VN="_ENV_MOD_${MOD}"
-    local FN=${!VN}
+    MOD=$1
+    VN="_ENV_MOD_${MOD}"
+    FN=${!VN}
 
-    if [ -z "${FN}" ]; then
+    if [[ -z ${FN} ]]; then
       echo "Special variable not defined or empty: ${VN}" >&2
       echo "In your module, use a:" >&2
       echo >&2
-      echo "  ${VN}=\${BASH_SOURCE[0]}" >&2
+      echo "  export ${VN}=\${BASH_SOURCE[0]}" >&2
       echo >&2
 
       return 1
     fi
   else
-    # Edit the "global" envfile.
+    # Edit the global envfile.
 
     # The flag to distinguish "global" mode.
-    local MOD=""
+    MOD=""
 
-    local FN=$(_lenv_fn); [ -n "${FN}" ] || return 1
+    FN=$(_lenv_fn) || return 1
   fi
 
   # Edit the envfile.
@@ -112,9 +90,11 @@ leed() {
   # NOTE: Some editors output a newline here. Not our glitch.
 
   # If a successful global edit, print loaded module names as a hint.
-  if [ -z "${MOD}" ]; then
+  if [[ -z ${MOD} ]]; then
     local MODS=$(lemod)
-    [ -n "${MODS}" ] && echo -e "Also, these modules have been loaded:\n\n${MODS}" >&2
+    [[ -n ${MODS} ]] && {
+      echo -e "Also, these modules have been loaded:\n\n${MODS}" >&2
+    }
   fi
 
   # Finally, source the envfile.
@@ -123,7 +103,57 @@ leed() {
 
 # List loaded modules.
 lemod() {
+  # AF: TODO: Better filtering of exported vars.
   declare -p \
   | grep "declare -- _ENV_MOD_" \
   | sed "s/^declare -- _ENV_MOD_//"
 }
+
+#--------------------------------------- Service
+
+# Locate and print the envfile path. Return 1 if not found.
+_lenv_fn() {
+  # Search for a readable envfile up the tree.
+
+  # AF: TODO: Что за хрень? CUP.
+  # Testing: `_LENV_FN_ROOT` is an optional root specifier.
+
+  local BN=$_LENV_BN
+  local D
+  echo -e "\e[1;32m${FUNCNAME[0]}():\e[0m hey:{}" >&2
+  D=$(realpath "${PWD}") || return 1
+
+  echo -e "\e[1;32m${FUNCNAME[0]}():\e[0m cp 1:{cp 1}" >&2
+
+  local TRY
+
+  # AF: TODO: CUP.
+  # while [[ -n ${D#${_LENV_FN_ROOT:-}} ]]; do
+  #   for TRY in "`realpath "${D}/../${D##*/}_support"`/${BN}" "`realpath "${D}/../_support"`/${BN}"; do
+  #     echo -e "\e[1;32m${FUNCNAME[0]}():\e[0m TRY:${TRY}"
+  #     if [[ -r ${TRY} ]]; then
+  #       echo "${TRY}"
+  #       return 0
+  #     fi
+  #   done
+
+  #   D=${D%/*}
+  # done
+
+  for TRY in "$(realpath "$D/../${D##*/}_support")/${BN}" "$(realpath "${D}/../_support")/${BN}"; do
+    echo -e "\e[1;32m${FUNCNAME[0]}():\e[0m TRY:${TRY}" >&2
+    if [[ -r ${TRY} ]]; then
+      echo "${TRY}"
+      return 0
+    fi
+  done
+
+  echo "Error: Support directory with a readable \`${BN}\` not found" >&2
+  return 1
+}
+
+#
+# Implementation notes:
+#
+# * `local VAR=` assignment yields a non-error even if the command has failed.
+#    Hence for result-checking clauses a separate `local VAR` is used.
